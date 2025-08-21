82fd1e0bd095c88c0c9b730622ff41f0148752b0
Boikhutso mashigo <boikhutsomashigo@gmail.com>
2025-08-21 20:22:09 +0200

    feature(main.py):implement the temp to be returned in °C

--- main.py
+++ main.py
@@ -5,13 +5,17 @@ API_KEY = "72ec2ba5feb448436283b37aa395cef7"
 
 CITY = input("enter the name of your city:  ")
 
-weather_data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={CITY}&units=imperial&APPID={API_KEY}")
+weather_data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={CITY}&units=metric&APPID={API_KEY}")
 
-weather = weather_data.json()['weather'][0]['main']
-temp = round(weather_data.json()['main']['temp'])
+if weather_data.json()['cod'] == '404':
+    print("No city found")
 
-print(f"The weather in {CITY} is: {weather}")
-print(f"the temperature in {CITY} is: {temp}")
+else:
+    weather = weather_data.json()['weather'][0]['main']
+    temp = round(weather_data.json()['main']['temp'])
+
+    print(f"The weather in {CITY} is: {weather}")
+    print(f"the temperature in {CITY} is: {temp}°C")
 
 
 
