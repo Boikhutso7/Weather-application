68df0721782df094aaca5ae4d6eb8efbd3935a66
Boikhutso mashigo <boikhutsomashigo@gmail.com>
2025-08-22 19:55:43 +0200

    fix(main.py): fix city not found error handling

--- main.py
+++ main.py
@@ -1,21 +1,15 @@
-
 import requests
 
-API_KEY = "72ec2ba5feb448436283b37aa395cef7"
 
-CITY = input("enter the name of your city:  ")
+API_KEY = "72ec2ba5feb448436283b37aa395cef7"
+CITY = input("Enter the name of your city: ")
 
 weather_data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={CITY}&units=metric&APPID={API_KEY}")
 
 if weather_data.json()['cod'] == '404':
     print("No city found")
-
 else:
     weather = weather_data.json()['weather'][0]['main']
     temp = round(weather_data.json()['main']['temp'])
-
     print(f"The weather in {CITY} is: {weather}")
     print(f"the temperature in {CITY} is: {temp}°C")
-
-
-
